95e11c0b95837822ba03fc1966e0d96a144f8b36
Rachiee <rachi7ace@gmail.com>
2025-09-02 14:51:54 +0100

    Update data.js

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ export const menuArray = [
         id: 0,
         price: 14,
         emoji: "🍕",
-        image: "./images/pizza.png"
+        image: "/images/pizza.png"
     },
     {
         name: "Hamburger",
@@ -13,7 +13,7 @@ export const menuArray = [
         price: 12,
         emoji: "🍔",
         id: 1,
-        image: "./images/burger.png"
+        image: "/images/burger.png"
     },
         {
         name: "Beer",
@@ -21,6 +21,6 @@ export const menuArray = [
         price: 12,
         emoji: "🍺",
         id: 2,
-        image: "./images/beer.png"
+        image: "/images/beer.png"
     }
 ]
